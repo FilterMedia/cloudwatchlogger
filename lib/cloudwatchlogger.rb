be60f0f4f4f385a478f1b445ea27d35847850847
David P. Smith <kcculhwch@gmail.com>
2021-09-09 19:51:24 -0400

    fix it

--- lib/cloudwatchlogger.rb
+++ lib/cloudwatchlogger.rb
@@ -7,11 +7,15 @@ module CloudWatchLogger
   class LogGroupNameRequired < ArgumentError; end
   class LogEventRejected < ArgumentError; end
 
+
+
   def self.new(credentials, log_group_name, log_stream_name = nil, opts = {})
     client = CloudWatchLogger::Client.new(credentials, log_group_name, log_stream_name, opts)
     logger = Logger.new(client)
     logger.class.include LoggerSilence
-
+    class << logger
+      attr_accessor :local_level
+    end
     if client.respond_to?(:formatter)
       logger.formatter = client.formatter(opts[:format])
     elsif client.respond_to?(:datetime_format)
