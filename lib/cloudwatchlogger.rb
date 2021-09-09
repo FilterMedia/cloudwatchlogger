deed3b0110b454a32ddde8f4eacd4a9795c736fe
David P. Smith <kcculhwch@gmail.com>
2021-09-09 19:27:36 -0400

    try including silencer here

--- lib/cloudwatchlogger.rb
+++ lib/cloudwatchlogger.rb
@@ -1,6 +1,7 @@
 require File.join(File.dirname(__FILE__), 'cloudwatchlogger', 'client')
 
 require 'logger'
+require "active_support/logger_silence"
 
 module CloudWatchLogger
   class LogGroupNameRequired < ArgumentError; end
@@ -9,6 +10,7 @@ module CloudWatchLogger
   def self.new(credentials, log_group_name, log_stream_name = nil, opts = {})
     client = CloudWatchLogger::Client.new(credentials, log_group_name, log_stream_name, opts)
     logger = Logger.new(client)
+    logger.class.include LoggerSilence
 
     if client.respond_to?(:formatter)
       logger.formatter = client.formatter(opts[:format])
